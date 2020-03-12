98b29abdeeda6d5c7973a726d384fc07b5913b94
Ahmad <aalkhald@ford.com>
2020-03-11 21:34:01 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TicketmasterApiService } from './ticketmaster-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stumble-app-final-project';
+  public sportsEvents;
+  public familyEvents;
+  public musicEvents;
+  public artEvents;
+
+  
+  constructor(private __TicketmasterApiService: TicketmasterApiService) { }
+
+  ngOnInit() {
+    // First 4 is just for logging. We can disable it
+    this.__TicketmasterApiService.getSports().subscribe(sports => console.log(sports['_embedded']['events']))
+    this.__TicketmasterApiService.getFamily().subscribe(family =>  console.log(family['_embedded']['events']))
+    this.__TicketmasterApiService.getMusic().subscribe(music => console.log(music['_embedded']['events']))
+    this.__TicketmasterApiService.getSports().subscribe(art => console.log(art['_embedded']['events']))
+
+    // Below 4 lines to get the data back to the html
+    // this.__TicketmasterApiService.getSports().subscribe(sports =>this.sportsEvents = sports['_embedded']['events'])
+    // this.__TicketmasterApiService.getFamily().subscribe(family =>this.familyEvents = family['_embedded']['events'])
+    // this.__TicketmasterApiService.getMusic().subscribe(music =>this.musicEvents = music['_embedded']['events'])
+    // this.__TicketmasterApiService.getSports().subscribe(art =>this.artEvents = art['_embedded']['events'])
+    
+  }
 }
