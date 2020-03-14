e0aee3b13e94277078e77b3ca8b6be2a47158c14
Ahmad <aalkhald@ford.com>
2020-03-14 19:05:50 -0400

    adding by city data

--- src/app/apikey.ts
+++ src/app/apikey.ts
@@ -1,3 +1,4 @@
 export const credentials = {
-    apiKey: 'oJg1ssT8GkVknKJ2kFY8qAx3Dzw4GeYd'
+    // apiKey: 'oJg1ssT8GkVknKJ2kFY8qAx3Dzw4GeYd',
+    apiKey: 'zgE7vAvkLIfsIIpuoTsD8Y7PGndTek6Y'
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,20 +18,33 @@ export class AppComponent {
   constructor(private __TicketmasterApiService: TicketmasterApiService) { }
 
 
-  ngOnInit() {
-    // Below 4 lines to get the data back to the html
-    // this.__TicketmasterApiService.getSports().subscribe(sports => this.sportsEvents = sports['_embedded']['events'])
+  async ngOnInit() {
+    // Below 4 lines to get the data back to the view on page load
+    //  this.__TicketmasterApiService.getSports().subscribe(sports => {
+    //   this.sportsEvents = sports['_embedded']['events'];
+    //    this.__TicketmasterApiService.getFamily().subscribe(family => {
+    //      this.familyEvents = family['_embedded']['events'];
+    //       this.__TicketmasterApiService.getMusic().subscribe(music => {
+    //         this.musicEvents = music['_embedded']['events'];
+    //         this.__TicketmasterApiService.getArt().subscribe(art => {
+    //           this.artEvents = art['_embedded']['events']
+    //         })
+    //       })
+    //    })
+    // })
+    this.__TicketmasterApiService.getSports().subscribe(sports => this.sportsEvents = sports['_embedded']['events'])
     this.__TicketmasterApiService.getFamily().subscribe(family =>this.familyEvents = family['_embedded']['events'])
-    // this.__TicketmasterApiService.getMusic().subscribe(music =>this.musicEvents = music['_embedded']['events'])
-    // this.__TicketmasterApiService.getArt().subscribe(art =>this.artEvents = art['_embedded']['events'])
+    this.__TicketmasterApiService.getMusic().subscribe(music =>this.musicEvents = music['_embedded']['events'])
+    this.__TicketmasterApiService.getArt().subscribe(art =>this.artEvents = art['_embedded']['events'])
   }
 
 
-  // onSearch(textSearch) {
-  //   this.__TicketmasterApiService.getSports(textSearch).subscribe(sports => this.sportsEvents = sports['_embedded']['events'])
-  //   this.__TicketmasterApiService.getFamily(textSearch).subscribe(family =>this.familyEvents = family['_embedded']['events'])
-  //   this.__TicketmasterApiService.getMusic(textSearch).subscribe(music =>this.musicEvents = music['_embedded']['events'])
-  //   this.__TicketmasterApiService.getArt(textSearch).subscribe(art =>this.artEvents = art['_embedded']['events'])
-  // }
+  // Below is only called when Customer searches for events in specific city
+  onSearch(textSearch) {
+    this.__TicketmasterApiService.getSports(textSearch).subscribe(sports => this.sportsEvents = sports['_embedded']['events'])
+    this.__TicketmasterApiService.getFamily(textSearch).subscribe(family =>this.familyEvents = family['_embedded']['events'])
+    this.__TicketmasterApiService.getMusic(textSearch).subscribe(music =>this.musicEvents = music['_embedded']['events'])
+    this.__TicketmasterApiService.getArt(textSearch).subscribe(art =>this.artEvents = art['_embedded']['events'])
+  }
 
 }
