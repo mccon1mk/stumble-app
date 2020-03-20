5e00b95410b963cf6d0aa28230c53fab5d337ba4
Mixie Clerard <clerardm@gmail.com>
2020-03-20 00:00:57 -0400

    card stack complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { TicketmasterApiService } from '../ticketmaster-api.service';
+import { Component, OnInit } from "@angular/core";
+import { TicketmasterApiService } from "../ticketmaster-api.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
-
 export class HomeComponent implements OnInit {
   sportsEvents;
   public familyEvents;
@@ -14,19 +13,28 @@ export class HomeComponent implements OnInit {
   public artEvents;
   public textSearch;
 
-
-  constructor(public __TicketmasterApiService: TicketmasterApiService) { }
-
+  constructor(public __TicketmasterApiService: TicketmasterApiService) {}
 
   ngOnInit() {
-    this.__TicketmasterApiService.getSports().subscribe(sports => this.sportsEvents = sports['_embedded']['events']);
-    setTimeout(() => { this.__TicketmasterApiService.getFamily().subscribe(family => { this.familyEvents = family['_embedded']['events'] }) }, 1000);
-    setTimeout(() => { this.__TicketmasterApiService.getMusic().subscribe(music => { this.musicEvents = music['_embedded']['events'] }) }, 2000);
-    setTimeout(() => { this.__TicketmasterApiService.getArt().subscribe(art => { this.artEvents = art['_embedded']['events'] }) }, 2000);
-    this.sportsEvents = this.__TicketmasterApiService.SportsArr;
+    this.__TicketmasterApiService
+      .getSports()
+      .subscribe(sports => (this.sportsEvents = sports["_embedded"]["events"]));
+    setTimeout(() => {
+      this.__TicketmasterApiService.getFamily().subscribe(family => {
+        this.familyEvents = family["_embedded"]["events"];
+      });
+    }, 1000);
+    setTimeout(() => {
+      this.__TicketmasterApiService.getMusic().subscribe(music => {
+        this.musicEvents = music["_embedded"]["events"];
+      });
+    }, 2000);
+    setTimeout(() => {
+      this.__TicketmasterApiService.getArt().subscribe(art => {
+        this.artEvents = art["_embedded"]["events"];
+      });
+    }, 2000);
   }
-
-
 
   // Below is only called when Customer searches for events in specific city
   onSearch(textSearch) {
@@ -35,14 +43,27 @@ export class HomeComponent implements OnInit {
     // this.__TicketmasterApiService.getMusic(textSearch).subscribe(music =>this.musicEvents = music['_embedded']['events'])
     // this.__TicketmasterApiService.getArt(textSearch).subscribe(art =>this.artEvents = art['_embedded']['events'])
 
-    this.__TicketmasterApiService.getSports(textSearch).subscribe(sports => this.sportsEvents = sports['_embedded']['events'])
-    setTimeout(() => { this.__TicketmasterApiService.getFamily(textSearch).subscribe(family => { this.familyEvents = family['_embedded']['events'] }) }, 1000);
-    setTimeout(() => { this.__TicketmasterApiService.getMusic(textSearch).subscribe(music => { this.musicEvents = music['_embedded']['events'] }) }, 2000);
-    setTimeout(() => { this.__TicketmasterApiService.getArt(textSearch).subscribe(art => { this.artEvents = art['_embedded']['events'] }) }, 2000)
+    this.__TicketmasterApiService
+      .getSports(textSearch)
+      .subscribe(sports => (this.sportsEvents = sports["_embedded"]["events"]));
+    setTimeout(() => {
+      this.__TicketmasterApiService.getFamily(textSearch).subscribe(family => {
+        this.familyEvents = family["_embedded"]["events"];
+      });
+    }, 1000);
+    setTimeout(() => {
+      this.__TicketmasterApiService.getMusic(textSearch).subscribe(music => {
+        this.musicEvents = music["_embedded"]["events"];
+      });
+    }, 2000);
+    setTimeout(() => {
+      this.__TicketmasterApiService.getArt(textSearch).subscribe(art => {
+        this.artEvents = art["_embedded"]["events"];
+      });
+    }, 2000);
   }
 
   delete(event) {
     this.sportsEvents.splice(event, 1);
   }
-
 }
