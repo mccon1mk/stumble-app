c82c96d6fab253b52baabc59fb259509e0f2eb25
Molly.McConnell <molly.mcconnell@mrm-mccann.com>
2020-04-08 00:59:21 -0400

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule, Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BucketListComponent } from './bucket-list/bucket-list.component';
 import { HomeComponent } from './home/home.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -34,9 +34,10 @@ import {MatIconModule} from '@angular/material/icon';
   ],
   entryComponents: [HomeComponent],
   bootstrap: [AppComponent],
-  providers: [{ 
-    provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, 
-    useValue: { appearance: 'fill' } },
+  providers: [{
+    provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+    useValue: { appearance: 'fill' }
+  },
   ]
 })
 export class AppModule { }
